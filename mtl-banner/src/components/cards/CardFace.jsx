fc2f932dccdd267ea7c20458abd51d2d4688e497
riiach <riiachoii@gmail.com>
2026-05-25 19:54:35 +0900

    Feat: Card positioning logic added

--- mtl-banner/src/components/cards/CardFace.jsx
+++ mtl-banner/src/components/cards/CardFace.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const CardFace = () => {
+const CardFace = ( {image, index, cardWidth, totalCards }) => {
+    const positionX = -(index * cardWidth); // Calculate the position based on the index
     return (
-        <div className="w-full h-full relative overflow-hidden">
-            <img
-                src=""
-                alt=""
-                className="absolute inset-0 w-full h-full object-cover"
-            />
-        </div>
+        <div className="w-full h-full bg-cover bg-no-repeat"
+             style={{
+                 backgroundImage: `url(${image})`,
+                 backgroundPosition: `${positionX}px center`,
+                 backgroundSize: `${cardWidth * totalCards}px 100%`,
+             }}
+        />
     )
 }
 export default CardFace
