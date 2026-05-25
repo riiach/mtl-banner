d5f5b5793cc01d8edf6f0a40add9385398a8a04b
riiach <riiachoii@gmail.com>
2026-05-25 19:13:11 +0900

    feat: Container added

---
+++ mtl-banner/src/components/layout/Container.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Container = () => {
+    return (
+        <div className="w-full h-full bg-white flex flex-col gap-2">
+            <h1 className="hidden font-mulish font-bold lg:text-4xl text-black md:block">
+                mtl
+            </h1>
+        </div>
+    )
+}
+export default Container
