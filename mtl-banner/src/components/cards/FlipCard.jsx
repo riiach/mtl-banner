9b7f746a5a195ab5621268529973483a116ea481
riiach <riiachoii@gmail.com>
2026-05-25 19:18:22 +0900

    Feat: Setting face logic implemented

--- mtl-banner/src/components/cards/FlipCard.jsx
+++ mtl-banner/src/components/cards/FlipCard.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FlipCard = () => {
+    const [faceIndex, setFaceIndex] = useState(0);
+
+    const nextFace = () => {
+        setFaceIndex((prevIndex) => (prevIndex + 1) % 3);
+    };
+
     return (
         <div className="flex-1 border border-red-500">
 
