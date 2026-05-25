1e6fc93c19cfa90aeccc5cbc5e4353b704bc3ebb
riiach <riiachoii@gmail.com>
2026-05-25 20:03:22 +0900

    Feat: Card index increasing logic added

--- mtl-banner/src/components/cards/cards.data.js
+++ mtl-banner/src/components/cards/cards.data.js
@@ -1,17 +1,10 @@
-export const cards = [
-    {
-        id: 1,
-        title: "Face1",
-        image: "/CardFace1.png"
-    },
-    {
-        id: 2,
-        title: "Face2",
-        image: "/CardFace2.png"
-    },
-    {
-        id: 3,
-        title: "Face3",
-        image: "/CardFace3.png"
-    }
-]
+export const images = [
+    "/CardFace1.png",
+    "/CardFace2.png",
+    "/CardFace3.png",
+];
+
+export const cards = Array.from({ length: 60 }, (_, i) => ({
+    id: i,
+    i,
+}))
