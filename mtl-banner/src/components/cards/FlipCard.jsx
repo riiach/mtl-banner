aee3fe0fc79c08d604a10da4afe57653182d565d
riiach <riiachoii@gmail.com>
2026-05-25 19:23:04 +0900

    Feat: Automatic Flipping and Hover Flipping system implemented

--- mtl-banner/src/components/cards/FlipCard.jsx
+++ mtl-banner/src/components/cards/FlipCard.jsx
@@ -2,10 +2,19 @@ import React, { useEffect, useState } from 'react'
 
 const FlipCard = () => {
     const [faceIndex, setFaceIndex] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
 
     const nextFace = () => {
         setFaceIndex((prevIndex) => (prevIndex + 1) % 3);
     };
+
+    useEffect(() => {
+        if (isHovered) return; // When hovered, don't automatically flip
+
+        const interval = setInterval(nextFace, 3000);
+
+        return () => clearInterval(interval);
+    }, [isHovered]);
 
     return (
         <div className="flex-1 border border-red-500">
